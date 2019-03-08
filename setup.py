2b6b5af8fbc50221f5595d8b3fa5156e44da01e7
Valentin Kulesh <leshy@massarak.sh>
2019-03-08 18:03:17 +0300

    Starting aggressive cleanup...

--- setup.py
+++ setup.py
@@ -1,53 +1,39 @@
-# git-remote-hg setuptools script
-
 import setuptools
-import subprocess
+
 import sys
-import os
+print sys.path
 
-# derive version from git repo
-cmd = ["git", "describe", "--tags"]
-commit = os.environ.get('REV', None)
-if commit:
-  cmd.append(commit)
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-version = process.communicate()[0].strip()
-# strip leading v
-version = version[1:]
-
-# check for released version
-assert (len(version) > 0)
-assert (version.find('-') < 0)
-
-long_description = \
-"""
-'git-remote-hg' is a gitremote protocol helper for Mercurial.
-It allows you to clone, fetch and push to and from Mercurial repositories as if
-they were Git ones using a hg::some-url URL.
-
-See the homepage for much more explanation.
-"""
-
-CLASSIFIERS = [
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "License :: OSI Approved",
-    "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-]
-
-setuptools.setup(name="git-remote-hg",
-      version=version,
-      author="Mark Nauwelaerts",
-      author_email="mnauw@users.sourceforge.net",
-      url="http://github.com/mnauw/git-remote-hg",
-      description="access hg repositories as git remotes",
-      long_description=long_description,
-      license="GPLv2",
-      keywords="git hg mercurial",
-      scripts=["git-remote-hg", "git-hg-helper"],
-      classifiers=CLASSIFIERS
-     )
-
+setuptools.setup(
+    name="git-remote-hg",
+    version='1.0.0',
+    author="Mark Nauwelaerts",
+    author_email="mnauw@users.sourceforge.net",
+    url="http://github.com/mnauw/git-remote-hg",
+    description="access hg repositories as git remotes",
+    long_description="""
+                 'git-remote-hg' is a git-remote protocol helper for Mercurial.
+                 It allows you to clone, fetch and push to and from Mercurial repositories as if
+                 they were Git ones using a hg::some-url URL.
+                 
+                 See the homepage for much more explanation.
+                 """,
+    license="GPLv2",
+    keywords="git hg mercurial",
+    packages=[
+        'git_remote_hg',
+    ],
+    scripts=[
+        'git-remote-hg',
+        'git-hg-helper',
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+    ],
+    zip_safe=False,
+)
